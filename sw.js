a2b89b9ccd59620f401af8b649b2b94db03cacae
dptc-admin <dptransmittercalc@gmail.com>
2025-06-25 17:08:37 -0500

    v1

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'dptc-app-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/icons/192.png',
+    '/icons/512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cached => {
+                // Return cache if hit, else fetch from network
+                return cached || fetch(event.request);
+            })
+    );
+});
